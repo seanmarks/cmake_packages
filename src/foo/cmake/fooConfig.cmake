5ea5c4d101a1fdc0feb56bf6ca02f07181b803f6
seanmarks <SeanMarks1123@gmail.com>
2020-07-02 11:16:23 -0400

    Fixed inclusion of header files as a CMake package.

--- src/foo/cmake/fooConfig.cmake
+++ src/foo/cmake/fooConfig.cmake
@@ -1,1 +1,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/fooTargets.cmake")
+
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+#include(${SELF_DIR}/mylib-targets.cmake)
+get_filename_component(foo_INCLUDE_DIRS "${SELF_DIR}/../../../include" ABSOLUTE)
